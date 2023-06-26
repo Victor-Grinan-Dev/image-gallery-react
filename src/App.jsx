bfdacfce465ba5981bc8ebc808c2a3deaab5fc06
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-06-26 13:52:16 +0300

    deploy ready

---
+++ src/App.jsx
@@ -0,0 +1,156 @@
+import "./App.css";
+import bookmark from "./images/bookmark.svg";
+import imageGallery from "./images/image-gallery.png";
+import image1 from "./images/image1.jpg";
+import image2 from "./images/image2.jpg";
+import image3 from "./images/image3.jpg";
+import image4 from "./images/image4.jpg";
+import image5 from "./images/image5.jpg";
+import image6 from "./images/image6.jpg";
+
+function App() {
+  return (
+    <div class="global-container flex items-center justify-center min-h-screen bg-cyan-50">
+      <div className="card-container bg-white p-6 m-3 space-y-10 rounded-3xl shadow-2xl md:p-40">
+        <div className="menu-container flex flex-col items-center justify-center space-y-3 md:flex-row md:space-y-0 md:space-x-8 md:mb-24 md:justify-end">
+          <div className="menu-items group">
+            <a href="/">Vector</a>
+            <div className="underline mx-2 mt-2 duration-500 border-b-2 opacity-0 border-black group-hover:opacity-100"></div>
+          </div>
+          <div className="menu-items group">
+            <a href="/">Ilustrations</a>
+            <div className="underline mx-2 mt-2 duration-500 border-b-2 opacity-0 border-black group-hover:opacity-100"></div>
+          </div>
+          <div className="menu-items group">
+            <a href="/">Images</a>
+            <div className="underline mx-2 mt-2 duration-500 border-b-2 opacity-0 border-black group-hover:opacity-100"></div>
+          </div>
+          <div className="menu-items group">
+            <a href="/">Icons</a>
+            <div className="underline mx-2 mt-2 duration-500 border-b-2 opacity-0 border-black group-hover:opacity-100"></div>
+          </div>
+        </div>
+
+        <div className="search-container flex flex-col md:flex-row justify-between space-y-5md:space-y-0">
+          <div className="search flex justify-between border-b">
+            <input
+              type="text"
+              className="ml-6 border-none md:w-80 placeholder:font-thin focus:outline-none"
+              placeholder="Search"
+            />
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-8 text-gray-300 duration-200 hover:scale-110"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <circle cx="10" cy="10" r="7" />
+                <line x1="21" y1="21" x2="15" y2="15" />
+              </svg>
+            </button>
+          </div>
+          <button className="upload-btn py-3 px-14 text-lg font-normal text-white bg-black border-black rounded-md shadow-2xl duration-200 hoverr:bg-white hover:text-black">
+            Upload
+          </button>
+        </div>
+
+        <div className="gallery-container grid gap-4 md:grid-cols-3">
+
+          {/* Items */}
+          <div className="image-1 relative group">
+            <img src={image1} alt="display" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="image-1 relative group">
+            <img src={image2} alt="display" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="image-1 relative group">
+            <img src={image3} alt="display" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="image-1 relative group">
+            <img src={image4} alt="display" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="image-1 relative group">
+            <img src={image5} alt="display" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="image-1 relative group">
+            <img src={image6} alt="display" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstract Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <img src={bookmark} alt="bookmark" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
